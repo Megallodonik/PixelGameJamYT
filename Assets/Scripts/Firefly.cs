79f87e5125a289508f3bf3006e3ddc71036974b5
Megallodonik <megallodonik@gmail.com>
2025-05-27 00:49:27 +0300

    Firefly first concept

---
+++ Assets/Scripts/Firefly.cs
@@ -0,0 +1,46 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Firefly : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+
+    public float MovementSpeed = 1f;
+
+    Vector3 targetPos;
+    void Start()
+    {
+        float posX = UnityEngine.Random.Range(-11f, 7f);
+        float posY = UnityEngine.Random.Range(-4f, 4f);
+
+
+        targetPos = new Vector3(posX, posY, 0);
+
+        Move();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        transform.position = Vector3.Lerp(pos, targetPos, 0.25f * Time.deltaTime);
+    }
+
+    async void Move()
+    {
+        
+
+        float posX = UnityEngine.Random.Range(-11f, 7f);
+        float posY = UnityEngine.Random.Range(-4f, 4f);
+
+
+        targetPos = new Vector3(posX, posY, 0);
+        Debug.Log(targetPos);
+
+        await UniTask.Delay(UnityEngine.Random.Range(1000, 5000)); // 1sec - 5 sec
+        Move();
+    }
+}
